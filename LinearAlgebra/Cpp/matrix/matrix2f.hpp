ab36f26bc908dd5ae1e32bd9f607ef76cf99eb13
ShingoHosoda <tomas0ark@gmail.com>
2021-06-02 14:42:35 +0900

    テストコードをassert文に切り替え

--- LinearAlgebra/Cpp/matrix/matrix2f.hpp
+++ LinearAlgebra/Cpp/matrix/matrix2f.hpp
@@ -5,6 +5,7 @@ namespace math
 {
   namespace mat2
   {
+    // 行列の相等
     bool equal(std::vector<double> matrix1, std::vector<double> matrix2)
     {
       bool flag = false;
@@ -31,6 +32,15 @@ namespace math
       return flag;
     }
 
+    // 2x2行列の作成
+    std::vector<double> create(double a11, double a12, double a21, double a22)
+    {
+      std::vector<double> matrix = {a11, a12, a21, a22};
+
+      return matrix;
+    }
+
+    // ゼロ行列
     std::vector<double> zero()
     {
       std::vector<double> matrix = {0, 0, 0, 0};
@@ -38,6 +48,15 @@ namespace math
       return matrix;
     }
 
+    // 単位行列
+    std::vector<double> identity()
+    {
+      std::vector<double> matrix = {1, 0, 0, 1};
+
+      return matrix;
+    }
+
+    // 行列の加法
     std::vector<double> add(std::vector<double> matrix1, std::vector<double> matrix2)
     {
       if (matrix1.size() != 4 && matrix2.size() != 4)
@@ -54,6 +73,7 @@ namespace math
       return matrix;
     }
 
+    // 行列の減法
     std::vector<double> sub(std::vector<double> matrix1, std::vector<double> matrix2)
     {
       if (matrix1.size() != 4 && matrix2.size() != 4)
@@ -66,6 +86,40 @@ namespace math
       {
         matrix.push_back(matrix1[count] - matrix2[count]);
       }
+
+      return matrix;
+    }
+
+    //行列のスカラー倍(行列の定数倍)
+    std::vector<double> scalar_multi(double s, std::vector<double> matrix1)
+    {
+      if (matrix1.size() != 4)
+      {
+        std::cout << "Error" << std::endl;
+      }
+
+      std::vector<double> matrix;
+      for (int count = 0; count < matrix1.size(); count++)
+      {
+        matrix.push_back(s * matrix1[count]);
+      }
+
+      return matrix;
+    }
+
+    // 行列の乗法
+    std::vector<double> multi(std::vector<double> matrix1, std::vector<double> matrix2)
+    {
+      if (matrix1.size() != 4)
+      {
+        std::cout << "Error" << std::endl;
+      }
+
+      std::vector<double> matrix = {0, 0, 0, 0};
+      matrix[0] = matrix1[0] * matrix2[0] + matrix1[1] * matrix2[2];
+      matrix[1] = matrix1[0] * matrix2[1] + matrix1[1] * matrix2[3];
+      matrix[2] = matrix1[2] * matrix2[0] + matrix1[3] * matrix2[2];
+      matrix[3] = matrix1[2] * matrix2[1] + matrix1[3] * matrix2[3];
 
       return matrix;
     }
--- LinearAlgebra/Cpp/testMatrix2f.cpp
+++ LinearAlgebra/Cpp/testMatrix2f.cpp
@@ -1,49 +1,89 @@
 #include <iostream>
+#include <cassert>
 #include "matrix/matrix2f.hpp"
 
-void testZeroMatrix2(){
-  auto zeroMatrix = math::mat2::zero();
-  for(int count = 0; count < zeroMatrix.size(); count++){
-    std::cout << zeroMatrix[count] << " ";
-  }
-  std::cout << std::endl;
-}
-
-void testAddMatrix2(){
-  std::vector<double> matrix1 = {10, 20, 30, 40};
-  std::vector<double> matrix2 = {50, 60, 70, 80};
-  auto matrix = math::mat2::add(matrix1, matrix2);
-  for(int count = 0; count < matrix.size(); count++){
-    std::cout << matrix[count] << " ";
-  }
-  std::cout << std::endl;
-}
-
-void testSubMatrix2(){
-  std::vector<double> matrix1 = {10, 20, 30, 40};
-  std::vector<double> matrix2 = {50, 60, 70, 80};
-  auto matrix = math::mat2::sub(matrix1, matrix2);
-  for(int count = 0; count < matrix.size(); count++){
-    std::cout << matrix[count] << " ";
-  }
-  std::cout << std::endl;
-}
-
-void testEqual(){
+void testEqual()
+{
   std::vector<double> matrix1_1 = {10, 20, 30, 40};
   std::vector<double> matrix2_1 = {50, 60, 70, 80};
   auto ans1 = math::mat2::equal(matrix1_1, matrix2_1);
-  std::cout << ans1 << std::endl;
+  assert(ans1 == false);
 
   std::vector<double> matrix1_2 = {10, 20, 30, 40};
   std::vector<double> matrix2_2 = {10, 20, 30, 40};
   auto ans2 = math::mat2::equal(matrix1_2, matrix2_2);
-  std::cout << ans2 << std::endl;
+  assert(ans2 == true);
 }
 
-int main(){
+void testCreateMatrix2(){
+  auto matrix = math::mat2::create(100, 200, 300, 400);
+  std::vector<double> answer = {100, 200, 300, 400}; 
+  assert(matrix.size() == 4);
+  assert(math::mat2::equal(matrix, answer) == true);
+}
+
+void testZeroMatrix2()
+{
+  auto zeroMatrix = math::mat2::zero();
+  std::vector<double> answer = {0, 0, 0, 0};
+  assert(zeroMatrix.size() == 4);
+  assert(math::mat2::equal(zeroMatrix, answer) == true);
+}
+
+void testIdentityMatrix2()
+{
+  auto identityMatrix = math::mat2::identity();
+  std::vector<double> answer = {1, 0, 0, 1};
+  assert(identityMatrix.size() == 4);
+  assert(math::mat2::equal(identityMatrix, answer) == true);
+}
+
+void testAddMatrix2()
+{
+  std::vector<double> matrix1 = {10, 20, 30, 40};
+  std::vector<double> matrix2 = {50, 60, 70, 80};
+  std::vector<double> answer = {60, 80, 100, 120};
+  auto matrix = math::mat2::add(matrix1, matrix2);
+  assert(matrix.size() == 4);
+  assert(math::mat2::equal(matrix, answer) == true);
+}
+
+void testSubMatrix2()
+{
+  std::vector<double> matrix1 = {10, 20, 30, 40};
+  std::vector<double> matrix2 = {50, 60, 70, 80};
+  std::vector<double> answer = {-40, -40, -40, -40};
+  auto matrix = math::mat2::sub(matrix1, matrix2);
+  assert(matrix.size() == 4);
+  assert(math::mat2::equal(matrix, answer) == true);
+}
+
+void testScalarMultiMatrix2(){
+  std::vector<double> matrix1 = {10, 20, 30, 40};
+  double s = 2;
+  std::vector<double> answer = {20, 40, 60, 80};
+  auto matrix = math::mat2::scalar_multi(s, matrix1);
+  assert(matrix.size() == 4);
+  assert(math::mat2::equal(matrix, answer) == true);
+}
+
+void testMultiMatrix2(){
+  std::vector<double> matrix1 = {1, 2, 3, 4};
+  std::vector<double> matrix2 = {5, 6, 7, 8};
+  std::vector<double> answer = {19, 22, 43, 50};
+  auto matrix = math::mat2::multi(matrix1, matrix2);
+  assert(matrix.size() == 4);
+  assert(math::mat2::equal(matrix, answer) == true);
+}
+
+int main()
+{
+  testEqual();
+  testCreateMatrix2();
   testZeroMatrix2();
+  testIdentityMatrix2();
   testAddMatrix2();
   testSubMatrix2();
-  testEqual();
+  testScalarMultiMatrix2();
+  testMultiMatrix2(); 
 }
